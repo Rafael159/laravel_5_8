933f48eb4f3e1a29c3ac2d512680bb7632c860f2
Rafael Alves <rafael-hyuga@hotmail.com>
2019-08-02 10:39:00 -0300

    +Refactoring code -added 'scope' to our model -refactored our store function, making it much more cleaner -used two ways to add massive data into the db (fillable|guarded)

--- Project-Mirage/app/Customer.php
+++ Project-Mirage/app/Customer.php
@@ -5,6 +5,24 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
-{
-    //
+{   
+    /**
+     * Fillable is the opposite of Guarded. Which means that in $fillable
+     * we declare what is expected to be filled. The other way around, 
+     * in $guarded, we declared which fields cannot be accepted
+     */
+
+    // Fillable Example
+    //protected $fillable = ['name', 'email', 'active'];
+
+    //Guarded Example
+    protected $guarded = [];
+
+    public function scopeActive($query){
+        return $query->where('active', 1);
+    }
+
+    public function scopeInactive($query){
+        return $query->where('active', 0);
+    }
 }
